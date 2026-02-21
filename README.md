a4c3ec2b8b4afca173461f5d4bc28dc599022396
nat-king-15 <natrajlilhare151@gmail.com>
2026-02-21 10:50:37 +0530

    chore: replace hardcoded variables with environment variables

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/SAURABHKUMARCHAUHAN1203/ANACONDA-PUBLIC)
-                     
+## NATKINGxtractorPro
+
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/nat-king-15/NATKINGxtractorPro)
--- config.py
+++ config.py
@@ -1,45 +1,22 @@
-"""
-from os import getenv
-
-
-API_ID = int(getenv("API_ID", "20937734"))
-API_HASH = getenv("API_HASH", "1db7bdcf908100cc641c6a5276765c3d")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "5121141243"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5121141243").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002226641720"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002226641720")) "22746239"))
-API_HASH = environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
-
-"""
-#
-
-
-
-
-# --------------M----------------------------------
-
 import os
 from os import getenv
-# ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "24473318"))
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5")
-# ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8085201369:AAGAch2LfumVz0ATPXvdC1VPQmWK5KZpfoo")
-# -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("TesttxcorestBot")
-# ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "5840594311"))
-# ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5840594311").split()))
+# ---------------CONFIG---------------------------------
+API_ID = int(os.environ.get("API_ID", "6886135"))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002674886829"))
+API_HASH = os.environ.get("API_HASH", "ee20a1c8a8e44eaa638b7254cbcc3012")
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://mongodburl9721:rfNOuo4c1OSTZ7cs@cluster0.edezeww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8525803667:AAEjpylk_qaZ62O_gIZmch46S8VQNYxcY1M")
+# ------------------------------------------------
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Natking_uploader_bot")
+# ------------------------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "2118600611"))
+# ------------------------------------------------
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "2118600611").split()))
+# ------------------------------------------------
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003744110162"))
+# ------------------------------------------------
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://nattu:nattu@cluster0.quvds.mongodb.net/?appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002674886829"))
-
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003744110162"))
